c7dcc3e06eaeac57bbde1135f0fbcd8c20e8e28b
AkumuKernel <50686824+AkumuKernel@users.noreply.github.com>
2021-10-17 01:39:09 -0300

    Create 01-hola-mundo.php

---
+++ PHP/01-hola-mundo.php
@@ -0,0 +1,20 @@
+<?php
+include "includes/header.php";
+echo("Hola mundo 1");
+?>
+
+<br>
+
+<?php
+echo "Hola mundo\n";
+
+print("Hola mundo");
+
+print("Hola mundo");
+
+print_r("Hola mundo");
+
+var_dump("Hola mundo");
+
+
+include "includes/footer.php";
